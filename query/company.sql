01af7044c1a2738f18428398ee6b46ed69f87b95
xiazemin <xiazemin@shimo.im>
2021-05-28 14:51:15 +0800

    /*  name: Companys :execresult */ select * from company where id BETWEEN ? AND ?;

--- query/company.sql
+++ query/company.sql
@@ -6,3 +6,9 @@ select companyName from company where companyName =?;
 
 /* name: InsertMulti :execresult */
 insert into company (id,name,companyName) values (?,?,?),(?,?,?);
+
+/*  name: CompanysRange :execresult */
+select * from company where id > ? AND  id < ?;
+
+/*  name: Companys :execresult */
+select * from company where id BETWEEN ? AND ?;
